4a4caa18fa67bc2151d085184ef760635acc13f3
Russ Paul-Jones <russ@paul-jones.org>
2021-01-31 13:07:40 -0800

    Getting a background to paint

--- LEDSim.py
+++ LEDSim.py
@@ -5,3 +5,22 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Color, Ellipse, Rectangle, Line
 from kivy.clock import Clock
 
+class LEDSim(App):
+	def build(self):
+		self.root = layout = GridLayout()
+		layout.bind(size=self._update_rect, pos=self._update_rect)
+
+		with layout.canvas.before:
+			Color(0.1, .9, 0.1, 1)  # green; colors range from 0-1 not 0-255
+			self.rect = Rectangle(size=layout.size, pos=layout.pos)
+			
+		return layout
+
+	def _update_rect(self, instance, value):
+		self.root.canvas.clear()
+		self.rect.pos = instance.pos
+		self.rect.size = instance.size
+
+	
+if __name__ == '__main__':
+	LEDSim().run()
