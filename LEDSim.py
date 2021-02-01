e5a7233ad5e9ec796c37120d0e531f716e9286f3
Russ Paul-Jones <russ@paul-jones.org>
2021-01-31 17:11:04 -0800

    Remove main window layout to allow widgets to show

--- LEDSim.py
+++ LEDSim.py
@@ -2,38 +2,47 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 from kivy.graphics import Color, Ellipse, Rectangle, Line
 from kivy.clock import Clock
 
+
 class ColorFill(Widget):
 	def __init__(self, **kwargs):
-		super(Widget, self).__init__(**kwargs)
-
-		with self.canvas:
-			Color(0.4, 0.4, 1.0, 1.0)
-			self.bg_rect = Rectangle(pos = self.pos, size = self.size)
+		super(ColorFill, self).__init__(**kwargs)
+		self.bind(pos=self.redraw, size=self.redraw)
 		return
 
-	def redraw(self):
+	def redraw(self, instance, value):
 		self.bg_rect.size = self.size
 		self.bg_rect.pos = self.pos
 		return
 
+	def setColor(self, red, green, blue, alpha):
+		self.canvas.clear()
+		with self.canvas.before:
+			Color(red, green, blue, alpha)
+			self.bg_rect = Rectangle(pos = self.pos, size = self.size)
+
 class LEDSim(App):
 	def build(self):
-		self.root = layout = GridLayout()
-		layout.bind(size=self._update_rect, pos=self._update_rect)
+		self.root = layout = GridLayout(cols=1)
+#		layout.bind(size=self._update_rect, pos=self._update_rect)
+		LED = ColorFill()
+		LED.setColor(0.4, 0.4, 1.0, 1.0)
+		layout.add_widget(LED)
 
-		with layout.canvas.before:
-			Color(0.1, .9, 0.1, 1)  # green; colors range from 0-1 not 0-255
-			self.rect = Rectangle(size=layout.size, pos=layout.pos)
+#		with layout.canvas.before:
+#			Color(0.1, .9, 0.1, 1)  # green; colors range from 0-1 not 0-255
+#			self.rect = Rectangle(size=layout.size, pos=layout.pos)
 			
 		return layout
 
-	def _update_rect(self, instance, value):
-		self.root.canvas.clear()
-		self.rect.pos = instance.pos
-		self.rect.size = instance.size
+#	def _update_rect(self, instance, value):
+#		self.root.canvas.clear()
+#		self.rect.pos = [instance.pos[0], instance.size[1]/2]
+#		size = [instance.size[0], instance.size[1]/2]
+#		self.rect.size = size
 
 	
 if __name__ == '__main__':
