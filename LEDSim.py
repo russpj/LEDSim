766cbe98123483c01ef98d98463f9573fa507d54
Russ Paul-Jones <russ@paul-jones.org>
2021-01-31 19:14:27 -0800

    Set size of LED grid so that LEDs are square

--- LEDSim.py
+++ LEDSim.py
@@ -7,28 +7,42 @@ from kivy.graphics import Color, Ellipse, Rectangle, Line
 from kivy.clock import Clock
 from ColorFill import ColorFill
 
-numStrips = 1
-lengthStrips = 20
+
+class MainLayout(FloatLayout):
+	def __init__(self, **kwargs):
+		super(MainLayout, self).__init__(**kwargs)
+
+
 
 class LEDSim(App):
 	def build(self):
-		self.root = layout = FloatLayout()
+		self.root = layout = MainLayout()
+
+		self.numStrips = 1
+		self.lengthStrips = 20
 		
-		self.grid = GridLayout(rows = numStrips)
+		self.grid = GridLayout(rows = self.numStrips)
 		layout.add_widget(self.grid)
 		self.LEDs = []
-		for stripNum in range(numStrips):
+		for stripNum in range(self.numStrips):
 			strip = []
-			for LEDLocation in range(lengthStrips):
+			for LEDLocation in range(self.lengthStrips):
 				LED = ColorFill()
-				brightness = LEDLocation / lengthStrips
+				brightness = LEDLocation / self.lengthStrips
 				LED.setColor(brightness, brightness, 1.0, 1.0)
 				self.grid.add_widget(LED)
 				strip.append(LED)
 			self.LEDs.append(strip)
+
+		layout.bind(size=self.update_layout)
 			
 		return layout
 
-	
+	def update_layout(self, instance, value):
+		squareSide = min(instance.size[0]/self.lengthStrips, instance.size[1]/self.numStrips)
+		gridWidth = squareSide*self.lengthStrips
+		gridHeight = squareSide*self.numStrips
+		self.grid.size_hint_max = (gridWidth, gridHeight)
+
 if __name__ == '__main__':
 	LEDSim().run()
