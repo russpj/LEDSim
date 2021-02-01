8150a0738dfccdc28a5c60f63803329184dba8c7
Russ Paul-Jones <russ@paul-jones.org>
2021-02-01 12:08:50 -0800

    Add Sample to Breathe module

--- Breathe.py
+++ Breathe.py
@@ -28,3 +28,44 @@ class Breathe:
 			self.brightness = 0.0
 			self.brightenAmount = -self.brightenAmount
 
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from ColorFill import ColorFill
+
+
+class Sample(App):
+	def build(self):
+		self.root = layout = FloatLayout()
+
+		self.numStrips = 1
+		self.lengthStrips = 20
+		
+		self.grid = GridLayout(rows = self.numStrips, pos_hint = {'center_x': .5, 'center_y': 0.5})
+		layout.add_widget(self.grid)
+
+		self.effects = []
+		for stripNum in range(self.numStrips):
+			strip = []
+			for LEDLocation in range(self.lengthStrips):
+				LED = ColorFill()
+				LED.setColor(0.0, 0.0, 0.0, 1.0)
+				self.grid.add_widget(LED)
+				strip.append(LED)
+			self.effects.append(Breathe(strip))
+
+		layout.bind(size=self.update_layout)
+
+		return layout
+
+	def update_layout(self, instance, value):
+		squareSide = min(instance.size[0]/self.lengthStrips, instance.size[1]/self.numStrips)
+		gridWidth = squareSide*self.lengthStrips
+		gridHeight = squareSide*self.numStrips
+		self.grid.size_hint_max = (gridWidth, gridHeight)
+		return
+
+if __name__ == '__main__':
+	Sample().run()
+
